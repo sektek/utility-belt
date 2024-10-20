c16da68f50763538b69e8588c76ee88f3123f132
Edward Kelly <eddie@sektek.net>
2024-10-20 16:56:46 +0000

    fix: Component should return never when T is never

--- src/types/component.ts
+++ src/types/component.ts
@@ -1,5 +1,7 @@
-export type Component<T, K extends keyof T> = T[K] extends (
-  ...args: infer A
-) => infer R
-  ? T | ((...args: A) => R) | R
-  : never;
+export type Component<T, K extends string> = T extends never
+  ? never
+  : K extends keyof T
+    ? T[K] extends (...args: infer A) => infer R
+      ? T | ((...args: A) => R)
+      : never
+    : never;
