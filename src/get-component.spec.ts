bc36929f1061f7024c0575a305bbce46f78abf13
Edward Kelly <eddie@sektek.net>
2025-05-31 22:28:09 +0000

    feat!: Updated getComponent to accept options instead of a fallback value.

--- src/get-component.spec.ts
+++ src/get-component.spec.ts
@@ -24,7 +24,7 @@ describe('getComponent', function () {
     expect(result).to.equal(fn);
   });
 
-  it('should throw an error when provided undefined without a fallback', function () {
+  it('should throw an error when provided undefined without options', function () {
     expect(() =>
       getComponent<{ fn: () => string } | (() => string), () => string>(
         undefined,
@@ -33,7 +33,7 @@ describe('getComponent', function () {
     ).to.throw('Invalid component');
   });
 
-  it('should throw an error when provided null without a fallback', function () {
+  it('should throw an error when provided null without options', function () {
     expect(() =>
       getComponent<{ fn: () => string } | (() => string), () => string>(
         null,
@@ -42,26 +42,56 @@ describe('getComponent', function () {
     ).to.throw('Invalid component');
   });
 
-  it('should return a function from an object given as fallback', function () {
+  it('should return a function from an object given as default', function () {
     const obj = { fn: () => 'value' };
 
     const result = getComponent<
       { fn: () => string } | (() => string),
       () => string
-    >(undefined, 'fn', obj);
+    >(undefined, 'fn', { default: obj });
 
     expect(result).to.be.a('function');
     expect(result.name).to.equal('bound fn');
     expect(result()).to.equal('value');
   });
 
-  it('should return a function that is given as fallback', function () {
+  it('should return a function from an object returned by a defaultProvider', function () {
+    const obj = { fn: () => 'value' };
+
+    const result = getComponent<
+      { fn: () => string } | (() => string),
+      () => string
+    >(undefined, 'fn', { defaultProvider: () => obj });
+
+    expect(result).to.be.a('function');
+    expect(result.name).to.equal('bound fn');
+    expect(result()).to.equal('value');
+  });
+
+  it('should return a function that is given as default', function () {
     const fn = () => {};
-    expect(getComponent(undefined, 'fn', fn)).to.equal(fn);
+    expect(getComponent(undefined, 'fn', { default: fn })).to.equal(fn);
+  });
+
+  it('should return a function from returned by a defaultProvider', function () {
+    const fn = () => 'default value';
+    expect(
+      getComponent(undefined, 'fn', { defaultProvider: () => fn }),
+    ).to.equal(fn);
   });
 
   it('should return the fallback when provided null', function () {
     const fn = () => {};
-    expect(getComponent(null, 'fn', fn)).to.equal(fn);
+    expect(getComponent(null, 'fn', { default: fn })).to.equal(fn);
+  });
+
+  it('should include the provided name in the error message', function () {
+    expect(() =>
+      getComponent<{ fn: () => string } | (() => string), () => string>(
+        undefined,
+        'fn',
+        { name: 'testName' },
+      ),
+    ).to.throw('Invalid testName');
   });
 });
--- src/get-component.ts
+++ src/get-component.ts
@@ -1,10 +1,42 @@
+import {
+  SyncProviderComponent,
+  SyncProviderFn,
+} from './types/sync-provider.js';
+import { InvalidComponentError } from './errors/invalid-component-error.js';
 import { isA } from './is-a.js';
 
+export type GetComponentOptions<T> = {
+  default?: T;
+  defaultProvider?: SyncProviderComponent<T>;
+  errorMessage?: string;
+  name?: string;
+};
+
+/**
+ * Retrieves a component function from the provided object, or acts as a pass-through
+ * if the object is a function. If the specified function name does not exist on the
+ * object, it will throw an error unless a default value is provided.
+ * @param obj - The object or function from which to retrieve the component.
+ *              If the object is a function, it will return the function directly.
+ * @param fnNames - The name(s) of the function(s) to retrieve from the object.
+ *                  If multiple names are provided, it will return the first one found.
+ * @param opts - Options to customize the behavior of the function retrieval.
+ *               - `default`: A default value to return if the function is not found.
+ *               - `defaultProvider`: A provider that can be used to get a default value.
+ *                 This should be a synchronous provider component that returns the default value.
+ *                 It is useful when the default value requires some initialization or computation.
+ *               - `errorMessage`: Custom error message to throw if the function is not found.
+ *               - `name`: A name to use in the error message for better context.
+ * @template T - The type of the object or function being passed.
+ * @template R - The type of the function being retrieved, which extends T and is a function.
+ * @throws {InvalidComponentError} If the function is not found and no default is provided.
+ * @returns {R} - The retrieved function, bound to the original object if applicable.
+ */
 // eslint-disable-next-line @typescript-eslint/ban-types
 export const getComponent = <T, R extends T & Function>(
   obj: T | null | undefined,
   fnNames: string | string[],
-  fallback?: T,
+  opts: GetComponentOptions<T> = {},
 ): R => {
   const result = [fnNames]
     .flat()
@@ -19,9 +51,24 @@ export const getComponent = <T, R extends T & Function>(
     return obj as R;
   }
 
-  if (fallback) {
-    return getComponent<T, R>(fallback, fnNames);
+  const errorMessage =
+    opts.errorMessage || `Invalid ${opts.name || 'component'}`;
+
+  if (opts.defaultProvider) {
+    const providerFn: SyncProviderFn<T> = getComponent(
+      opts.defaultProvider,
+      'get',
+    );
+    return getComponent<T, R>(providerFn(), fnNames, {
+      errorMessage: `Default Provider: ${errorMessage}`,
+    });
   }
 
-  throw new Error('Invalid component');
+  if (opts.default) {
+    return getComponent<T, R>(opts.default, fnNames, {
+      errorMessage: `Default Value: ${errorMessage}`,
+    });
+  }
+
+  throw new InvalidComponentError(obj, errorMessage);
 };
