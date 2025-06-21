bb3d902ce9bfe7495e23247bb70355a9e0d8af1e
Edward Kelly <eddie@sektek.net>
2025-06-21 14:27:41 +0000

    feat: update ProviderFn and Provider interfaces to support argument types

--- src/types/provider.ts
+++ src/types/provider.ts
@@ -1,9 +1,23 @@
 import { Component } from './component.js';
 
-export type ProviderFn<T> = () => T | PromiseLike<T>;
+/**
+ * A provider is a function that returns a value. It may expect an argument
+ * or not. It may be asynchronous, returning a Promise that resolves to the
+ * value.
+ * @example
+ * ```ts
+ * const getValue: Provider<number> = () => 42;
+ * const getValueWithArg: Provider<number, string> = (arg) => arg.length;
+ * ```
+ * @param R - The type of the value returned by the provider.
+ * @param T - The type of the argument passed to the provider.
+ *            If not provided, the provider does not expect an argument.
+ */
+export type ProviderFn<R, T = void> = (arg: T | void) => R | PromiseLike<R>;
 
-export interface Provider<T> {
-  get: ProviderFn<T>;
+/** An interface allowing for a class based provider */
+export interface Provider<R, T = void> {
+  get: ProviderFn<R, T>;
 }
 
-export type ProviderComponent<T> = Component<Provider<T>, 'get'>;
+export type ProviderComponent<R, T = void> = Component<Provider<R, T>, 'get'>;
