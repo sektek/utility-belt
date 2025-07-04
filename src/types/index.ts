bd388a02f366788a10e3ced2fc9b8ad71fe24806
Edward Kelly <eddie@sektek.net>
2025-07-04 21:10:17 +0000

    feat: refactor BuilderOptions and Builder types for improved functionality

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,4 @@
 export * from './builder.js';
-export * from './builder-options.js';
 export * from './component.js';
 export * from './event-emitting-service.js';
 export * from './execution-strategy.js';
