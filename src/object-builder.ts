b8237a8991aba07e1c70c9e48d245689d695e38b
Edward Kelly <eddie@sektek.net>
2025-07-04 21:20:42 +0000

    feat: enhance ObjectBuilder with detailed documentation

--- src/object-builder.ts
+++ src/object-builder.ts
@@ -2,20 +2,59 @@ import _ from 'lodash';
 
 import { Builder, BuilderOptions } from './types/builder.js';
 
+/**
+ * Options for the ObjectBuilder.
+ *
+ * @template T The type of object to build. Defaults to Record<string, unknown>.
+ * @property {Partial<BuilderOptions<T>>} [defaults] - Default values for the
+ * object properties. These can be functions that return values or
+ * static values.
+ * @property {Array<keyof T>} [copyKeys] - Keys to copy from the input
+ * object when calling `from()`. This allows for selective copying of
+ * properties from an existing object to the builder's defaults.
+ * If not provided, no keys are copied.
+ */
 export type ObjectBuilderOptions<T extends object = Record<string, unknown>> = {
   defaults?: Partial<BuilderOptions<T>>;
   copyKeys?: Array<keyof T>;
 };
 
+/**
+ * A builder for creating objects with specified properties and default values.
+ * This class allows you to define default values for object properties,
+ * as well as copy existing properties from another object.
+ * It supports both static values and functions that return values,
+ * enabling dynamic object creation.
+ *
+ * @template T The type of object to build.
+ */
 export class ObjectBuilder<T extends object> implements Builder<T> {
   #defaults: Partial<BuilderOptions<T>>;
   #copyKeys: Array<keyof T>;
 
+  /**
+   * Creates an instance of ObjectBuilder.
+   *
+   * @param {ObjectBuilderOptions<T>} [opts] - Options for the builder.
+   * @property {Partial<BuilderOptions<T>>} [opts.defaults] - Default values
+   * for the object properties.
+   * @property {Array<keyof T>} [opts.copyKeys] - Keys to copy from an
+   * existing object when calling `from()`.
+   */
   constructor(opts: ObjectBuilderOptions<T> = {}) {
     this.#defaults = opts.defaults ?? {};
     this.#copyKeys = opts.copyKeys ?? [];
   }
 
+  /**
+   * Creates a new ObjectBuilder from an existing object.
+   * The new builder will have the defaults merged with the properties
+   * of the provided object, allowing for selective copying of properties.
+   *
+   * @param {unknown} object - The object to copy properties from.
+   * @returns {ObjectBuilder<T>} A new ObjectBuilder instance with the
+   * copied properties.
+   */
   from(object: unknown): ObjectBuilder<T> {
     if (!(typeof object === 'object' && object !== null)) {
       throw new Error(
@@ -28,6 +67,15 @@ export class ObjectBuilder<T extends object> implements Builder<T> {
     });
   }
 
+  /**
+   * Creates a new ObjectBuilder with additional defaults.
+   * This allows for extending the existing defaults with new values.
+   *
+   * @param {Partial<BuilderOptions<T>>} [defaults] - Additional default values
+   * to merge with the existing defaults.
+   * @returns {ObjectBuilder<T>} A new ObjectBuilder instance with the
+   * merged defaults.
+   */
   with(defaults: Partial<BuilderOptions<T>> = {}): ObjectBuilder<T> {
     return new ObjectBuilder({
       defaults: { ...this.#defaults, ...defaults },
@@ -35,6 +83,17 @@ export class ObjectBuilder<T extends object> implements Builder<T> {
     });
   }
 
+  /**
+   * Creates an object with the specified properties and default values.
+   * The properties can be overridden by providing an object with the same
+   * keys. If a value is a function, it will be called with the override
+   * object as an argument.
+   *
+   * @param {Partial<BuilderOptions<T>>} [overrides] - Properties to override
+   * the defaults. If a property is a function, it will be called with the
+   * overrides as an argument.
+   * @returns {Promise<T>} A promise that resolves to the created object.
+   */
   async create(overrides: Partial<BuilderOptions<T>> = {}): Promise<T> {
     const result: Record<string, unknown> = {};
     await Promise.all(
@@ -80,6 +139,10 @@ export class ObjectBuilder<T extends object> implements Builder<T> {
     return override ?? value;
   }
 
+  /**
+   * Gets the creator function for the builder.
+   * @returns {(overrides: Partial<BuilderOptions<T>>) => Promise<T>} The creator function.
+   */
   get creator() {
     return this.create.bind(this);
   }
