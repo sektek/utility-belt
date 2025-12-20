57f9b42c58e6860b83f8711f19985e78283d0a54
Edward Kelly <eddie@sektek.net>
2025-12-20 14:19:45 +0000

    feat: ExecutionSet created to track execution within parallel execution strategy

---
+++ src/execution-strategies/execution-set.ts
@@ -0,0 +1,36 @@
+/**
+ * A utility class to manage a set of executing promises,
+ * allows for adding and deleting promises as a normal Set would,
+ * but also allows for "deleting" promises that were never added,
+ * preventing them from being added in the future.
+ */
+export class ExecutionSet {
+  executing: Set<Promise<unknown>> = new Set();
+  deletedExecuting: Set<Promise<unknown>> = new Set();
+
+  add(promise: Promise<unknown>) {
+    if (this.deletedExecuting.has(promise)) {
+      this.deletedExecuting.delete(promise);
+      return;
+    }
+
+    this.executing.add(promise);
+  }
+
+  delete(promise: Promise<unknown>) {
+    if (this.executing.has(promise)) {
+      this.executing.delete(promise);
+      return;
+    }
+
+    this.deletedExecuting.add(promise);
+  }
+
+  get size(): number {
+    return this.executing.size;
+  }
+
+  [Symbol.iterator]() {
+    return this.executing[Symbol.iterator]();
+  }
+}
