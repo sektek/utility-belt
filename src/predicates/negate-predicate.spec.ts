93f0a770507e27fbca05849bfa51dc6e8ff7052c
Edward Kelly <eddie@sektek.net>
2026-03-05 05:16:09 +0000

    chore: one more NegatePredicate spec fix.

--- src/predicates/negate-predicate.spec.ts
+++ src/predicates/negate-predicate.spec.ts
@@ -3,18 +3,18 @@ import { expect } from 'chai';
 import { NegatedPredicate } from './negate-predicate.js';
 
 describe('NegatedPredicate', function () {
-  it('should create an instance and negate the predicate result', function () {
+  it('should create an instance and negate the predicate result', async function () {
     const isNegative = new NegatedPredicate({
       predicate: (num: number) => num < 0,
     });
-    expect(isNegative.test(-1)).to.be.false;
-    expect(isNegative.test(1)).to.be.true;
+    expect(await isNegative.test(-1)).to.be.false;
+    expect(await isNegative.test(1)).to.be.true;
   });
 
-  it('should wrap a predicate component', function () {
+  it('should wrap a predicate component', async function () {
     const isZero = { test: (num: number) => num === 0 };
     const negatedIsZero = NegatedPredicate.wrap(isZero);
-    expect(negatedIsZero.test(0)).to.be.false;
-    expect(negatedIsZero.test(1)).to.be.true;
+    expect(await negatedIsZero.test(0)).to.be.false;
+    expect(await negatedIsZero.test(1)).to.be.true;
   });
 });
