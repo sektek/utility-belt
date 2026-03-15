3d81045ee8500aa2e5e701e9ab4d1ef99cfda9ca
Edward Kelly <eddie@sektek.net>
2026-03-15 19:41:05 +0000

    feat: create ArrayCollection as a Collection implementation

---
+++ src/array-collection.ts
@@ -0,0 +1,22 @@
+import { Collection } from './types/collection.js';
+
+/**
+ * A simple implementation of a collection that stores items in an array.
+ *
+ * @template T The type of item to be collected and provided.
+ */
+export class ArrayCollection<T> implements Collection<T> {
+  #items: T[] = [];
+
+  add(item: T) {
+    this.#items.push(item);
+  }
+
+  clear() {
+    this.#items = [];
+  }
+
+  values(): Iterable<T> {
+    return this.#items[Symbol.iterator]();
+  }
+}
