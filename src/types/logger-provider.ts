7083fc2c348d3105ce011e4f9a3950cdcde0cf50
Edward Kelly <eddie@sektek.net>
2026-04-05 15:27:03 +0000

    chore: use LoggerContext instead of Record<string, unknown> directly.

--- src/types/logger-provider.ts
+++ src/types/logger-provider.ts
@@ -1,28 +1,35 @@
+import { Logger, LoggerContext } from './logger.js';
 import {
   SyncProvider,
   SyncProviderComponent,
   SyncProviderFn,
 } from './sync-provider.js';
-import { Logger } from './logger.js';
+
+export type LoggerProviderOptions<T = void> = {
+  /* The context to use for this provider. */
+  context?: LoggerContext;
+  /* The context providers to use for this provider. */
+  contextProviders?: LoggerContextProviderComponent<T>[];
+};
 
 export type LoggerContextProviderFn<T = void> = SyncProviderFn<
-  Record<string, unknown>,
+  LoggerContext,
   T
 >;
 
 export interface LoggerContextProvider<T = void> extends SyncProvider<
-  Record<string, unknown>,
+  LoggerContext,
   T
 > {}
 
 export type LoggerContextProviderComponent<T = void> = SyncProviderComponent<
-  Record<string, unknown>,
+  LoggerContext,
   T
 >;
 
 export interface LoggerProvider<T = void> extends SyncProvider<Logger, T> {
   with(
-    context: Record<string, unknown>,
+    context: LoggerContext,
     ...contextProviders: LoggerContextProviderComponent<T>[]
   ): LoggerProvider<T>;
 }
